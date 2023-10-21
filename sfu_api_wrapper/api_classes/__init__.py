f3c8761e9f2ae66a449c828826e2221f031a6547
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-10-21 15:36:02 -0700

    feat: connect everything together

---
+++ sfu_api_wrapper/api_classes/__init__.py
@@ -0,0 +1,3 @@
+from .course import CourseOutline
+from .instructor import Instructor
+from .schedule import CourseSchedule
--- sfu_api_wrapper/requests.py
+++ sfu_api_wrapper/requests.py
@@ -1,16 +1,18 @@
 import aiohttp
-import asyncio
+import json
+
 
 class CourseOutlinesAPI:
     base_url = "http://www.sfu.ca/bin/wcm/course-outlines"
 
-    async def callApi(self, url):
+    @staticmethod
+    async def call_api(url):
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.get(url) as response:
                     if response.status == 200:
                         course_data = await response.text()
-                        return course_data
+                        return json.loads(course_data)
                     elif response.status == 404:
                         print("Course information not found.")
                         return None
@@ -20,76 +22,33 @@ class CourseOutlinesAPI:
             except aiohttp.ClientError as e:
                 print(f"Request error: {e}")
                 return None
-
-    async def get_years(self):
-        url = f"{self.base_url}"
-        return await self.callApi(url)
-
-    async def get_terms(self, year):
-        url = f"{self.base_url}?{year}"
-        return await self.callApi(url)
-
-    async def get_departments(self, year, term):
-        url = f"{self.base_url}?{year}/{term}"
-        return await self.callApi(url)
-
-    async def get_course_numbers(self, year, term, department):
-        url = f"{self.base_url}?{year}/{term}/{department}"
-        return await self.callApi(url)
-
-    async def get_course_sections(self, year, term, department, course_number):
-        url = f"{self.base_url}?{year}/{term}/{department}/{course_number}"
-        return await self.callApi(url)
-
-    async def get_course_outline(self, year, term, department, course_number, course_section):
-        url = f"{self.base_url}?{year}/{term}/{department}/{course_number}/{course_section}"
-        return await self.callApi(url)
-
-class Requests:
 
     @staticmethod
-    async def fetch_course_information_async(url: str):
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(url) as response:
-                    if response.status == 200:
-                        course_data = await response.text()
-                        return course_data
-                    elif response.status == 404:
-                        print("Course information not found.")
-                        return None
-                    else:
-                        print(f"API request failed with status code: {response.status}")
-                        return None
-            except aiohttp.ClientError as e:
-                print(f"Request error: {e}")
-                return None
+    async def get_years():
+        url = f"{CourseOutlinesAPI.base_url}"
+        return await CourseOutlinesAPI.call_api(url)
 
-# Example usage
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    api = CourseOutlinesAPI()
+    @staticmethod
+    async def get_terms(year):
+        url = f"{CourseOutlinesAPI.base_url}?{year}"
+        return await CourseOutlinesAPI.call_api(url)
 
-    # Get the list of years
-    years = loop.run_until_complete(api.get_years())
-    print("Years:", years)
+    @staticmethod
+    async def get_departments(year, term):
+        url = f"{CourseOutlinesAPI.base_url}?{year}/{term}"
+        return await CourseOutlinesAPI.call_api(url)
 
-    # Get the list of terms for a specific year
-    terms = loop.run_until_complete(api.get_terms("2015"))
-    print("Terms:", terms)
+    @staticmethod
+    async def get_course_numbers(year, term, department):
+        url = f"{CourseOutlinesAPI.base_url}?{year}/{term}/{department}"
+        return await CourseOutlinesAPI.call_api(url)
 
-    # Get the list of departments for a specific year and term
-    departments = loop.run_until_complete(api.get_departments("2015", "summer"))
-    print("Departments:", departments)
+    @staticmethod
+    async def get_course_sections(year, term, department, course_number):
+        url = f"{CourseOutlinesAPI.base_url}?{year}/{term}/{department}/{course_number}"
+        return await CourseOutlinesAPI.call_api(url)
 
-    # Get the list of course numbers for a specific year, term, and department
-    course_numbers = loop.run_until_complete(api.get_course_numbers("2015", "summer", "cmpt"))
-    print("Course Numbers:", course_numbers)
-
-    # Get the list of course sections for a specific year, term, department, and course number
-    course_sections = loop.run_until_complete(api.get_course_sections("2015", "summer", "cmpt", "110"))
-    print("Course Sections:", course_sections)
-
-    # Get a specific course outline for a given year, term, department, course number, and section
-    course_outline = loop.run_until_complete(api.get_course_outline("2015", "summer", "cmpt", "110", "c100"))
-    print("Course Outline:", course_outline)
+    @staticmethod
+    async def get_course_outline(year, term, department, course_number, course_section):
+        url = f"{CourseOutlinesAPI.base_url}?{year}/{term}/{department}/{course_number}/{course_section}"
+        return await CourseOutlinesAPI.call_api(url)
