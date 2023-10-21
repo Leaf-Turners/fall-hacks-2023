f5e30c26cfd56aa2d4aa6a295574a1c708a18a3a
Nolan Campbell <nolancampbell3@gmail.com>
2023-10-21 14:01:38 -0700

    square brackets

--- sfu_api_wrapper/api-classes/schedule.py
+++ sfu_api_wrapper/api-classes/schedule.py
@@ -1,10 +1,10 @@
 class CourseOrExam:
     def __init__(self, raw_data):
-        self.start_time = raw_data.startTime
-        self.start_date = raw_data.startDate
-        self.end_time = raw_data.endTime
-        self.end_date = raw_data.endDate
-        self.section_code = raw_data.sectionCode
-        self.is_exam = raw_data.isExam
-        self.days = raw_data.days
-        self.campus = raw_data.campus
+        self.start_time = raw_data['startTime']
+        self.start_date = raw_data['startDate']
+        self.end_time = raw_data['endTime']
+        self.end_date = raw_data['endDate']
+        self.section_code = raw_data['sectionCode']
+        self.is_exam = raw_data['isExam']
+        self.days = raw_data['days']
+        self.campus = raw_data['campus']
