cf1d71acef082fe68b512ce35b39e7c6f3336dca
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-10-21 13:49:46 -0700

    dist: make the repo a python package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+name = "sfuapiwrapper"
+version = "1.0.0"
+description = "SFU API wrapper."
+author = "Leaf Turners"
+url = "https://github.com/Leaf-Turners/fall-hacks-2023"
+long_description = "SFU API wrapper."
+
+install_requires = [
+]
+
+packages = find_packages()
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    author=author,
+    url=url,
+    long_description=long_description,
+    packages=packages,
+    install_requires=install_requires,
+)
---
+++ sfu_api_wrapper/wrapper_functions/course_offering.py
@@ -0,0 +1,2 @@
+async def course_offering(department: str, number: str, section: str, year: int = 'current', term: str = 'current'):
+    print('course offering')
