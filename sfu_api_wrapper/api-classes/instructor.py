ad8d0668e0fc78af8149b68a47af3766a0af432c
Nolan Campbell <nolancampbell3@gmail.com>
2023-10-21 13:44:11 -0700

    add classes with properties

---
+++ sfu_api_wrapper/api-classes/instructor.py
@@ -0,0 +1,12 @@
+class instructor:
+    def __init__(self, raw_data):
+        self.name = raw_data.name
+        self.last_name = raw_data.lastName
+        self.first_name = raw_data.firstName
+        self.common_name = raw_data.commonName
+        self.office = raw_data.office
+        self.office_hours = raw_data.officeHours
+        self.email = raw_data.email
+        self.phone = raw_data.phone
+        self.role_code = raw_data.roleCode
+        self.profile_url = raw_data.profileUrl
