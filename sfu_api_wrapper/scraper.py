0e8c52f69d1b45684e1c14c73ed5cc34ca1feb43
Adam <afb2@sfu.ca>
2023-10-21 19:46:03 -0700

    docStringsscraper

--- sfu_api_wrapper/scraper.py
+++ sfu_api_wrapper/scraper.py
@@ -14,6 +14,21 @@ with open(json_file_path, 'r') as json_file:
 
 
 async def fetch_course_data(session, course_id):
+    
+    """
+    This function fetches course data from CourseDiggers using the provided session and course ID.
+    
+    Args:
+        session (aiohttp.ClientSession): An aiohttp session for making HTTP requests.
+        course_id (int): The unique identifier for the course on CourseDiggers.
+
+    Returns:
+        json dictionary: Based on the session and course_id, return the json containing the course data,
+        such as keys that include the coursename, the course median grade and the fail rate.
+        The data requested returns None if the data cannot be fetched and we cannot retrieve data 
+        from CourseDiggers at that specific ID.
+    """
+    
     url = COURSE_DIGGER_JSON_URL.format(course_id)
     async with session.get(url) as response:
         if response.status == 200:
@@ -22,6 +37,19 @@ async def fetch_course_data(session, course_id):
 
 
 async def median_getter(course_name):
+    
+    """
+    Retrieves the median grade for a given course and returns it as a string.
+
+    Args:
+        course_name (str): The course name in the format "DEPARTMENT COURSE_NUMBER," e.g., "CMPT 310."
+
+    Returns:
+        str: If the formatted course name was a key inside our dictionary in the JSON file course_name_to_id, we'd retrieve the value
+        which is the Course Digger website id. At this website id, we retrieve the course name and median grade, e.g., "Course Name: CMPT 310, Median Grade: 80.5."
+        Otherwise, it returns an error message if the course data cannot be fetched.
+    """
+    
     if course_name in course_name_to_id:
         course_id = course_name_to_id[course_name]
         async with aiohttp.ClientSession() as session:
@@ -37,9 +65,20 @@ async def median_getter(course_name):
         return "Course name not found in the dictionary."
 
 async def failRate(department: str, course_number: str) -> str:
+    
     """
-    retrive the fail rate for the course 
+    Retrieves the fail rate for a given course and returns it as a string.
+
+    Args:
+        department (str): The department abbreviation, e.g., "CMPT."
+        course_number (str): The course number, e.g., "310."
+
+    Returns:
+        str: If the formatted department and course_number was a key inside our dictionary in the JSON file course_name_to_id, we'd retrieve the value
+        which is the Course Digger website id. At this website id, we retrieve a string containing the fail rate for the course.
+        Otherwise, returns an error message if the course data cannot be fetched.
     """
+    
     department = department.upper()
     course_name = department + " " + course_number
     if course_name in course_name_to_id:
@@ -58,8 +97,20 @@ async def failRate(department: str, course_number: str) -> str:
     
 async def courseDiggerInfo(department: str, course_number: str) -> dict:
     """
-    get json file for for the course digger information
+    Retrieves detailed course information from CourseDiggers API.
+
+    Args:
+        department (str): The department abbreviation, e.g., "CMPT."
+        course_number (str): The course number, e.g., "310."
+
+    Returns:
+        dict: If the formatted department and course_number was a key inside our dictionary in the JSON file course_name_to_id, we'd retrieve the value
+        which is the Course Digger website id. At this website id, we retrieve a 
+        dictionary containing detailed course information where the
+        keys may include 'name' and 'data' for course name and grade information.
+        Otherwise, returns None if the data cannot be fetched.
     """
+    
     department = department.upper()
     course_name = department + " " + course_number
     if course_name in course_name_to_id:
@@ -74,9 +125,3 @@ async def courseDiggerInfo(department: str, course_number: str) -> dict:
     else:
         return "Course name not found in the dictionary."
 
-    
-if __name__ == '__main__':
-    #user_input = input("Enter a course name: ")
-    result = asyncio.run(courseDiggerInfo('cmpt', '310'))
-
-    print(result)
